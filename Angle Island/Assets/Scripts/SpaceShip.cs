ac2e5d2c39ea4cde70b4c9ae23a0ef3fd4ccd178
fedepvg <federicopvg@gmail.com>
2019-05-10 13:00:52 -0300

    rotate function

--- Angle Island/Assets/Scripts/SpaceShip.cs
+++ Angle Island/Assets/Scripts/SpaceShip.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SpaceShip : MonoBehaviour
 {
@@ -13,27 +15,51 @@ public class SpaceShip : MonoBehaviour
     private float ZRotationRate;
     float InputX;
     float InputZ;
-    Vector2 MoveInput;
-    Vector3 MoveDir=Vector3.zero;
+    Transform [] positions;
+    [SerializeField]
+    private InputField InputText;
+    public Text BAckgroundText;
+    public float RotSpeed;
+    char ActualAxis;
+    Quaternion DestRot;
+
     void Awake()
     {
         rigi = GetComponentInParent<Rigidbody>();
         SpeedZ = 20f;
         XRotationRate = 70f;
         ZRotationRate = -70f;
+        DestRot = Quaternion.identity;
     }
 
     private void FixedUpdate()
-    {
-        GetInput(ref InputX,ref InputZ);
-        
-
-        rigi.AddRelativeTorque(InputX * XRotationRate * Time.deltaTime, 0f, InputZ * ZRotationRate * Time.deltaTime, ForceMode.Force);
-
-        rigi.AddRelativeForce(new Vector3(0, 0, 10 * SpeedZ));
+    {        
+        rigi.AddRelativeForce(new Vector3(0, 0, 10 * SpeedZ),ForceMode.Force);
 
         Debug.Log(rigi.velocity.sqrMagnitude);
         Debug.DrawRay(transform.position, transform.forward*10);
+    }
+
+    private void Update()
+    {
+        GetInput(ref InputX, ref InputZ);
+        transform.Rotate(InputX * XRotationRate * Time.deltaTime, 0f, InputZ * ZRotationRate * Time.deltaTime);
+        if(Input.GetKey(KeyCode.X))
+        {
+            Rotate(90, 'x');
+        }
+        else if(Input.GetKey(KeyCode.Y))
+        {
+            Rotate(90, 'y');
+        }
+        else if(Input.GetKey(KeyCode.Z))
+        {
+            Rotate(90, 'z');
+        }
+        else
+        {
+            Rotate();
+        }
     }
 
     void GetInput(ref float InputX,ref float InputZ)
@@ -55,6 +81,36 @@ public class SpaceShip : MonoBehaviour
         if (LayerMask.LayerToName(collision.gameObject.layer) == "Floor")
         {
             rigi.useGravity = true;
+        }
+    }
+
+    public void Rotate(int rot, char axis)
+    {
+        //DestRot=Quaternion.identity;
+        if (axis != ActualAxis)
+        {
+            switch (axis)
+            {
+                case 'x':
+                    DestRot *= Quaternion.AngleAxis(rot, transform.right);
+                    break;
+                case 'y':
+                    DestRot *= Quaternion.AngleAxis(rot, transform.up);
+                    break;
+                case 'z':
+                    DestRot *= Quaternion.AngleAxis(rot, transform.forward);
+                    break;
+            }
+        }
+        DestRot = DestRot * transform.rotation;
+    }
+
+    public void Rotate()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, DestRot, RotSpeed * Time.deltaTime);
+        if (transform.rotation == DestRot)
+        {
+            DestRot = Quaternion.identity;
         }
     }
 }
